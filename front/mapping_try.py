032c60538120826b8f6b77e37385c07478c2ff2b
Lee-Gahyeon0 <dlrkgus0858@gmail.com>
2025-05-24 00:02:06 +0900

    add initial files
    - mapping from select

--- front/mapping_try.py
+++ front/mapping_try.py
@@ -1,21 +1,16 @@
-import streamlit as st
-import pandas as pd
-import folium
-from streamlit_folium import folium_static
-from folium.plugins import MarkerCluster
 
-def render_map():
-    st.title("GilDam")
+def render_map(selected_places, csv_path="data/busan_spots.cvs"):
+    import streamlit as st
+    import pandas as pd
+    import folium
+    from streamlit_folium import folium_static
+    from folium.plugins import MarkerCluster
 
-    try:
-        df = pd.read_csv(r"C:\Users\leegh\Desktop\Gildam\데이터 셋/Busn_Spotdat.csv", encoding='utf-8')
-    except UnicodeDecodeError:
-        df = pd.read_csv(r"C:\Users\leegh\Desktop\Gildam\데이터 셋/Busn_Spotdat.csv", encoding='cp949')
-
-    st.dataframe(df, height=204)
-
+    df = pd.read_csv(cvs_path)
+    df = df[df["여행지"].isin(selected_places)]
     df[["lat","lon"]] = df[["위도","경도"]]
-
+    
+    #맵생성
     m = folium.Map(
         location=[35.07885, 129.04402],
         zoom_start=13,
@@ -23,12 +18,10 @@ def render_map():
         preset="barcelona",
         dpi=200)
 
-
-
-
+    #마커달기
     marker_cluster = MarkerCluster().add_to(m)
 
-    for idx, row in df.iterrows():
+    for _, row in df.iterrows():
         folium.Marker(
             location=[row["lat"], row["lon"]],
             popup=row["여행지"],
